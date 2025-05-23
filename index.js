fa40ee7a8529636863c47fba9abf1946b19e6ae2
NightKnight32 <92237560+NightKnight32@users.noreply.github.com>
2025-05-23 14:33:57 -0400

    UnderstandBasics

---
+++ index.js
@@ -0,0 +1,16 @@
+// This is a single-line comment
+      console.log("Hello, World!");
+
+      /*
+       * This is a multi-line comment
+       * It can span multiple lines
+       */
+      console.log("This is a multi-line comment");
+
+      let name = "John Doe"; // This is a String variable declaration
+      let age = 30; // This is number variable declaration
+      let isStudent = false; // This is a boolean variable declaration
+      let hobbies = ["reading", "gaming", "coding"]; // This is an array variable declaration
+      let firstName = null; // This is a null variable declaration
+      let lastName; // This is an undefined variable declaration
+      console.log(name); // undefined
